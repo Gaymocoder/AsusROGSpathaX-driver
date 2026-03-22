dd35894935d748d0e128d39a1b3c579f4c64b6fe
Gaymocoder <gaymocoder@gmail.com>
2026-03-22 03:51:44 +0300

    B&W | Added assertion macro
    — Added custon assertion macro with CRITICAL() logging;
    — Added "utils::macro"-header;
    
    — Builds and works properly.

---
+++ include/utils/macro.h
@@ -0,0 +1,15 @@
+#ifndef __AROGSX_UTILS_MACTO_H__
+#define __AROGSX_UTILS_MACTO_H__
+
+#include "utils/logger.h"
+
+#define VENDOR_ID  0x0b05
+#define PRODUCT_ID 0x1977
+
+#define gcs_assert(cond, fmt, ...) \
+    if (!cond) { \
+        CRITICAL(fmt __VA_OPT__(,) __VA_ARGS__); \
+        return 1; \
+    }
+
+#endif
